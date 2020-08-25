6c38a5da643f4fa1c844d169ac246587adae49c3
Luis Baldiviezo <luis@codefabric.io>
2020-08-25 01:11:52 -0400

    added ui deployment to serverless

--- src/apiOperations.js
+++ src/apiOperations.js
@@ -1,7 +1,7 @@
 const client = require("axios");
 
-const REQUEST_ENDPOINT = `https://p6mk0wglk9.execute-api.us-east-1.amazonaws.com/dev/otp/request`;
-const VERIFY_ENDPOINT = `https://p6mk0wglk9.execute-api.us-east-1.amazonaws.com/dev/otp/verify`;
+const REQUEST_ENDPOINT = `${process.env.REACT_APP_API_URL}/otp/request`;
+const VERIFY_ENDPOINT = `${process.env.REACT_APP_API_URL}/otp/verify`;
 
 export const requestOtp = async (email) => {
   const response = await client.post(REQUEST_ENDPOINT, {
